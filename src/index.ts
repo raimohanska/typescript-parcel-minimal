896ebd904482a94364840e850705fa36328e6aa5
Juha Paananen <juha.paananen@gmail.com>
2020-12-22 16:13:03 +0200

    minimal parcel setup for client-side typescript learning

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import _ from "lodash"
+
+// That's an example dependency over there ^
+
+console.log("Hello from TypeScript");
+console.log("Make some changes here and see if the watch->compile->reload cycle works");
